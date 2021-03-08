13cabfa8cd71103b7c70b1c24bc82dffaf2d060e
Ali Bas <alibas01@gmail.com>
2021-03-08 17:38:27 -0500

    validation tests ready up to step 3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,63 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    it "is not valid if password doesn't exist" do
-      a_user = User.new
-      expect(a_user).to_not be_valid
+
+    it "is saves all fields exist" do
+      a_user = User.new(first_name:"Ali", last_name:"Bas", email: "ali@ali.com", password:"12qw", password_confirmation:"12qw")
+      a_user.save
+      expect(a_user.save).to eq(true)
     end
+
+    it "is not valid if first name doesn't exist" do
+      a_user = User.new(last_name:"Bas", email: "ali@ali.com", password:"12qw", password_confirmation:"12qw")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
+    it "is not valid if last name doesn't exist" do
+      a_user = User.new(first_name:"Ali", email: "ali@ali.com", password:"12qw", password_confirmation:"12qw")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
+    it "is not valid if email doesn't exist" do
+      a_user = User.new(first_name:"Ali", last_name:"Bas", password:"12qw", password_confirmation:"12qw")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
+    it "is not valid if email is not unique" do
+      b_user = User.new(first_name:"Ali", last_name:"Bas", email: "ali@ali.com", password:"12qw", password_confirmation:"12qw")
+      b_user.save
+      a_user = User.new(first_name:"Ali", last_name:"Bas", email: "ali@ali.com", password:"12qw", password_confirmation:"12qw")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
+    it "is not valid if password doesn't exist" do
+      a_user = User.new(first_name:"Ali", last_name:"Bas", email: "ali@ali.com", password_confirmation:"12qw")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
+    it "is not valid if password-confirmation doesn't exist" do
+      a_user = User.new(first_name:"Ali", last_name:"Bas", email: "ali@ali.com", password:"12qw")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
+    it "is not valid if password doesn't match" do
+      a_user = User.new(first_name:"Ali", last_name:"Bas", email: "ali@ali.com", password:"12qw", password_confirmation:"wertqw")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
+    it "is not valid if password valid if password is shorter than 4 chars" do
+      a_user = User.new(first_name:"Ali", last_name:"Bas", email: "ali@ali.com", password:"12q", password_confirmation:"12q")
+      a_user.save
+      expect(a_user.save).to eq(false)
+    end
+
   end
 
 
