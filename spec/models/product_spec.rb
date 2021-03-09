a09eaf555b3f5ef7865ef5c31c5c03c216b041dc
Ali Bas <alibas01@gmail.com>
2021-03-08 20:10:05 -0500

    rspec validation all pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,33 +3,37 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
-    context "name exists" do
-      it "is not valid if name doesn't exist" do
-        a_product = Product.new(name:nil)
-        expect(a_product).to_not be_valid
-      end
+    it "is gonna be saved" do
+      @category = Category.new(name: "test")
+      a_product = Product.new(name:"slippers", price:25, quantity:4,category:@category)
+      expect(a_product.save).to eq(true)
     end
 
-    context "price exists" do
-      it "is not valid if price doesn't exist" do
-        a_product = Product.new(price:nil)
-        expect(a_product).to_not be_valid
-      end
+    it "is not valid if name doesn't exist" do
+      @category = Category.new(name: "test")
+      a_product = Product.new(name:nil, price:25, quantity:4,category:@category)
+      a_product.save
+      expect(a_product.errors.full_messages).to include("Name can't be blank")
     end
-
-    context "quantity exists" do
-      it "is not valid if quantity doesn't exist" do
-        a_product = Product.new(quantity:nil)
-        expect(a_product).to_not be_valid
-      end
+  
+    it "is not valid if price doesn't exist" do
+      @category = Category.new(name: "test")
+      a_product = Product.new(name:"slippers", quantity:4,category:@category)
+      a_product.save
+      expect(a_product.errors.full_messages).to include("Price can't be blank")
     end
-
-    context "category exists" do
-      it "is not valid if quantity doesn't exist" do
-        a_product = Product.new(category:nil)
-        expect(a_product).to_not be_valid
-      end
+  
+    it "is not valid if quantity doesn't exist" do
+      @category = Category.new(name: "test")
+      a_product = Product.new(name:"slippers", price:25,category:@category)
+      a_product.save
+      expect(a_product.errors.full_messages).to include("Quantity can't be blank")
     end
-
+    it "is not valid if category doesn't exist" do
+      @category = Category.new(name: "test")
+      a_product = Product.new(name:"slippers", price:25, quantity:4)
+      a_product.save
+      expect(a_product.errors.full_messages).to include("Category can't be blank")
+    end
   end
 end
